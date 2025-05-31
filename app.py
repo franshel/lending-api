23d2f9319d4c8efc539363f1fa7e55e1ca587709
Nicky Franshel <nickyfranshel@gmail.com>
2025-05-31 09:31:12 +0700

    test2

--- app.py
+++ app.py
@@ -72,13 +72,13 @@ origins = [
     "http://localhost:8080",
     "https://dingdong.loans"
     "http://dingdong.loans"
-    "*",  # Allow all origins (consider restricting in production)
+    # "*",  # Allow all origins (consider restricting in production)
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    # allow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
